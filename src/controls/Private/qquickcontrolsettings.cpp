b8443b33793bea16f244b515b7ebcd25bd78598b
Tomasz Olszak <olszak.tomasz@gmail.com>
2013-12-03 00:36:00 +0100

    Tizen: Settings.hasTouchScreen returns true on Tizen.
    Change-Id: I818bff2fcc416215c07b65e6714fcc995e9d3409
    Reviewed-by: Jarosław Staniek <staniek@kde.org>

--- src/controls/Private/qquickcontrolsettings.cpp
+++ src/controls/Private/qquickcontrolsettings.cpp
@@ -51,7 +51,7 @@ QT_BEGIN_NAMESPACE
 static QString defaultStyleName()
 {
     //Only enable QStyle support when we are using QApplication
-#if !defined(Q_OS_IOS) && !defined(Q_OS_ANDROID) && !defined (Q_OS_BLACKBERRY)
+#if !defined(Q_OS_IOS) && !defined(Q_OS_ANDROID) && !defined (Q_OS_BLACKBERRY) && !defined(Q_OS_LINUX_TIZEN)
     if (QCoreApplication::instance()->inherits("QApplication"))
         return QLatin1String("Desktop");
 #endif
@@ -74,7 +74,7 @@ static bool fromResource(const QString &path)
 bool QQuickControlSettings::hasTouchScreen() const
 {
 // QTBUG-36007
-#if defined(Q_OS_ANDROID)
+#if defined(Q_OS_ANDROID) || defined(Q_OS_LINUX_TIZEN)
     return true;
 #else
     foreach (const QTouchDevice *dev, QTouchDevice::devices())
